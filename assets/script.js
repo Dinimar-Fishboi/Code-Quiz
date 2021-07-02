1d37589542b394c6c47a90baab7031e28213dbb5
Dinimar Fish <tayla@tallisman.com.au>
2021-07-02 21:52:10 +1000

    have made the startQuizBtn summon a single random question at a time, as well as making that question go after an answer is selected.

--- assets/script.js
+++ assets/script.js
@@ -11,10 +11,40 @@ var secondsLeft = 60;
 
 
 var questionOne= { 
-    thisIsTheQuestion: "What is 2 + 2?",
-    possibleAnswers: ["5","4","8","22"],
+    thisIsTheQuestion: "Question 1",
+    possibleAnswers: ["wrong","4","wrong","wrong"],
     correctAnswer: "4", 
 } 
+
+var questionTwo = {
+  thisIsTheQuestion: "Question 2",
+  possibleAnswers: ["wrong","wrong","8","wrong"],
+  correctAnswer: "8", 
+}
+
+var questionThree = {
+  thisIsTheQuestion: "Question 3?",
+  possibleAnswers: ["wrong","16","wrong","wrong"],
+  correctAnswer: "16", 
+}
+
+var questionFour = {
+  thisIsTheQuestion: "Question 4?",
+  possibleAnswers: ["wrong","wrong","wrong","44"],
+  correctAnswer: "44", 
+}
+
+questionArray = [questionOne, questionTwo, questionThree, questionFour]
+
+
+function shuffleArray() {
+  questionArray.sort(() => Math.random() - 0.5);
+}
+
+shuffleArray();
+
+console.log(questionArray[1].thisIsTheQuestion)
+
 
 function setTime() {
     // Sets interval in variable
@@ -39,22 +69,33 @@ setTime();
 
    startQuizBtn.addEventListener("click",function launchQuiz (event) {
     event.preventDefault();
+    shuffleArray();
+
+    //function cardFlip(event) {
+    
+    for (i = 0; i < questionArray.length ; i++) {
+
+    // We are going to have a small group of random questions that are going 
+    // to be randomly generated each round.
+    // To do this we will bring in our little friend, the SHUFFLE ARRAY
 
     // What we want to do here is apply questionOne.thisIsTheQuestion to a 
     // h2 element (which still needs to be created in Index.html), and add
     // questionOne .possibleAnswers to a ul and li file.
     //document.holdQuestions.appendChild(questionOne );
-    document.getElementById('questionTitle').innerHTML = questionOne.thisIsTheQuestion;
-        
-        var answerList = document.getElementById("answerList");
+   document.getElementById('questionTitle').innerHTML = questionArray[1].thisIsTheQuestion;
+      console.log(questionArray[1])    
+
+        var answerList = document.createElement("ul");
+        holdQuestions.appendChild(answerList);
         console.log(event.target)
-        for (i = 0; i < questionOne.possibleAnswers.length ; i++) {
+        for (i = 0; i < 4 ; i++) {
             
             var liElement = document.createElement('li');
 
-            console.log(questionOne.possibleAnswers[i]);
-            liElement.textContent = questionOne.possibleAnswers[i]
-            liElement.innerHTML = questionOne.possibleAnswers[i]
+            console.log(questionArray[1].possibleAnswers[i]);
+            liElement.textContent = questionArray[1].possibleAnswers[i]
+            liElement.innerHTML = questionArray[1].possibleAnswers[i]
 
             console.log(answerList);
             answerList.appendChild(liElement);
@@ -63,16 +104,20 @@ setTime();
             liElement.addEventListener('click', function(event){
               var chosenAnswer = event.target.textContent;
               console.log(chosenAnswer)
-              console.log(questionOne.correctAnswer)
+              console.log(questionArray[1].correctAnswer)
               
-              if (event.target.textContent === questionOne.correctAnswer){
+              if (event.target.textContent === questionArray[1].correctAnswer){
                 console.log("That's it!");
+
               } else { console.log("Nope");
               }
 
+              holdQuestions.removeChild(answerList)
+              document.getElementById('questionTitle').innerHTML = "";
+
             })
         }
-         
+      }
         // When one liElement is clicked, we need an event listener to hear the click
         // How does the parent react when the li is clicked?
         // Remember event.target
