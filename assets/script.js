f39303e998e1d614d13413788fd4b8b221f77ba0
Dinimar Fish <tayla@tallisman.com.au>
2021-07-03 13:52:09 +1000

    created function quizBegins and placed the funtion that actually starts the quiz at the bottom of the js file

--- assets/script.js
+++ assets/script.js
@@ -5,6 +5,7 @@ var startQuizBtn = document.querySelector("#startQuiz");
 var holdQuestions = document.querySelector("#holdQuestions")
 var mainTitle = document.querySelector("#main")
 // var questionTitle = document.querySelector('#questionTitle')
+var questionNumber = 0;
 
 var liElement =0;
 
@@ -40,6 +41,10 @@ questionArray = [questionOne, questionTwo, questionThree, questionFour]
 
 function shuffleArray() {
   questionArray.sort(() => Math.random() - 0.5);
+  questionOne.possibleAnswers.sort(() => Math.random() - 0.5);
+  questionTwo.possibleAnswers.sort(() => Math.random() - 0.5);
+  questionThree.possibleAnswers.sort(() => Math.random() - 0.5);
+  questionFour.possibleAnswers.sort(() => Math.random() - 0.5);
 }
 
 shuffleArray();
@@ -55,6 +60,10 @@ function setTime() {
         // Stops execution of action at set interval
         clearInterval(timerInterval);
         document.getElementById("main").style.display = 'block'
+        
+        document.getElementById('exampleList').innerHTML = "";
+
+        document.getElementById('questionTitle').innerHTML = "";
         // Calls function to create and append image
       //  sendMessage();
       }
@@ -67,92 +76,158 @@ setTime();
 
 
 
-   startQuizBtn.addEventListener("click",function launchQuiz (event) {
-    event.preventDefault();
-    shuffleArray();
-    document.getElementById("main").style.display = 'none'
-
+ function quizBegins(){
 
     //function cardFlip(event) {
     
-  //for (i = 0; i < questionArray.length ; i++) {
+  for (i = 0; i < questionArray.length ; i++) {
 
-    // We are going to have a small group of random questions that are going 
-    // to be randomly generated each round.
-    // To do this we will bring in our little friend, the SHUFFLE ARRAY
+      console.log(questionArray[i])    
 
-    // What we want to do here is apply questionOne.thisIsTheQuestion to a 
-    // h2 element (which still needs to be created in Index.html), and add
-    // questionOne .possibleAnswers to a ul and li file.
-    //document.holdQuestions.appendChild(questionOne );
-
-      console.log(questionArray[0])    
-
-        var answerList = document.createElement("ul");
-        holdQuestions.appendChild(answerList);
-        console.log(event.target)
         for (g = 0; g < 4 ; g++) {
             
-            document.getElementById('questionTitle').innerHTML = questionArray[0].thisIsTheQuestion;
+            document.getElementById('questionTitle').innerHTML = questionArray[i].thisIsTheQuestion;
 
             var liElement = document.createElement('li');
+            var answerList = document.getElementById("exampleList")
 
-            liElement.innerHTML = questionArray[0].possibleAnswers[g]
+            liElement.innerHTML = questionArray[i].possibleAnswers[g]
 
             answerList.appendChild(liElement);
-            
+        }
+
             liElement.addEventListener('click', function(event){
               
-              if (event.target.textContent === questionArray[0].correctAnswer) {
+              if (event.target.textContent === questionArray[i].correctAnswer) {
                 console.log("That's it!");
 
               } else { console.log("Nope");
               secondsLeft = secondsLeft - 15;
             }
-          //    document.getElementById("main").style.display = 'block'
 
-              answerList.remove()
+
+              document.getElementById('exampleList').innerHTML = "";
+
               document.getElementById('questionTitle').innerHTML = "";
             })
+          
 
           // Second cycle
 
-            liElement.addEventListener('click', function(event){
-              var answerList = document.createElement("ul");
-              holdQuestions.appendChild(answerList);
-              console.log(event.target)
-              for (g = 0; g < 4 ; g++) {
+            // liElement.addEventListener('click', function(event){
+            //   var answerList = document.createElement("ul");
+            //   holdQuestions.appendChild(answerList);
+            //   console.log(event.target)
+            //   for (g = 0; g < 4 ; g++) {
                   
-                  document.getElementById('questionTitle').innerHTML = questionArray[1].thisIsTheQuestion;
+            //       document.getElementById('questionTitle').innerHTML = questionArray[1].thisIsTheQuestion;
       
-                  var liElement = document.createElement('li');
+            //       var liElement = document.createElement('li');
       
-                  liElement.innerHTML = questionArray[1].possibleAnswers[g]
+            //       liElement.innerHTML = questionArray[1].possibleAnswers[g]
       
-                  answerList.appendChild(liElement);
+            //       answerList.appendChild(liElement);
 
-                  liElement.addEventListener('click', function(event){
+            //       liElement.addEventListener('click', function(event){
               
-                    if (event.target.textContent === questionArray[1].correctAnswer) {
-                      console.log("That's it!");
+            //         if (event.target.textContent === questionArray[1].correctAnswer) {
+            //           console.log("That's it!");
       
-                    } else { console.log("Nope");
-                    secondsLeft = secondsLeft - 15;
-                  }
-                    document.getElementById("main").style.display = 'block'
+            //         } else { console.log("Nope");
+            //         secondsLeft = secondsLeft - 15;
+            //       }
+            //       //  document.getElementById("main").style.display = 'block'
       
-                    answerList.remove()
-                    document.getElementById('questionTitle').innerHTML = "";
-                  })
-              }
+            //       document.getElementById('exampleList').innerHTML = "";
+            //       document.getElementById('questionTitle').innerHTML = "";
+            //       })
 
-            })
-         }
+            //       // Third Cycle
 
-        
+                              
+            //       console.log(questionArray[2])    
+
+            //           var answerList = document.createElement("ul");
+            //           holdQuestions.appendChild(answerList);
+            //           console.log(event.target)
+            //           for (g = 0; g < 4 ; g++) {
+                          
+            //               document.getElementById('questionTitle').innerHTML = questionArray[2].thisIsTheQuestion;
+
+            //               var liElement = document.createElement('li');
+
+            //               liElement.innerHTML = questionArray[2].possibleAnswers[g]
+
+            //               answerList.appendChild(liElement);
+                          
+            //               liElement.addEventListener('click', function(event){
+                            
+            //                 if (event.target.textContent === questionArray[2].correctAnswer) {
+            //                   console.log("That's it!");
+
+            //                 } else { console.log("Nope");
+            //                 secondsLeft = secondsLeft - 15;
+            //               }
+            //         //    document.getElementById("main").style.display = 'block'
+
+            //         document.getElementById('exampleList').innerHTML = "";
+            //         document.getElementById('questionTitle').innerHTML = "";
+            //           })
+
+            //               // Fourth and final cycle
+
+            //               console.log(questionArray[3])    
+
+            //                 var answerList = document.createElement("ul");
+            //                 holdQuestions.appendChild(answerList);
+            //                 console.log(event.target)
+            //                 for (g = 0; g < 4 ; g++) {
+                                
+            //                     document.getElementById('questionTitle').innerHTML = questionArray[3].thisIsTheQuestion;
+
+            //                     var liElement = document.createElement('li');
+
+            //                     liElement.innerHTML = questionArray[3].possibleAnswers[g]
+
+            //                     answerList.appendChild(liElement);
+                                
+            //                     liElement.addEventListener('click', function(event){
+                                  
+            //                       if (event.target.textContent === questionArray[3].correctAnswer) {
+            //                         console.log("That's it!");
+
+            //                       } else { console.log("Nope");
+            //                       secondsLeft = secondsLeft - 15;
+            //                     }
+            //                       document.getElementById("main").style.display = 'block'
+            //                       // Will also need to tally final score as seconds left
+            //                       document.getElementById('exampleList').innerHTML = "";
+            //                       document.getElementById('questionTitle').innerHTML = "";
+            //                     })
+                    // }
+                    // }
+                              
+            //     }
+
+            // })
+          }
+
+        }
 
         // When one liElement is clicked, we need an event listener to hear the click
         // How does the parent react when the li is clicked?
         // Remember event.target
   
-      });
+      
+
+      startQuizBtn.addEventListener("click",function launchQuiz (event) {
+        event.preventDefault();
+        shuffleArray();
+        document.getElementById("main").style.display = 'none'
+        quizBegins();
+
+        // This is where we place the secondsLeft = highscore variable. 
+        // Also probably the localstorage?
+        document.getElementById("main").style.display = 'block'
+
+       });
