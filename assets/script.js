7bbe5463bf8939b571d50b6935ee779e6a9ce3c4
Dinimar Fish <tayla@tallisman.com.au>
2021-07-04 18:37:49 +1000

    have attempted to resolve timer not reverting to 0 when all questions are answered, to no result

--- assets/script.js
+++ assets/script.js
@@ -13,6 +13,7 @@ var liElement =0;
 var secondsLeft = 0;
 
 
+
 var questionOne= { 
     thisIsTheQuestion: "Question 1",
     possibleAnswers: ["wrong","4","wrong","wrong"],
@@ -53,184 +54,163 @@ shuffleArray();
 
 
 
-      startQuizBtn.addEventListener("click",function launchQuiz (event) {
-        event.preventDefault();
-        shuffleArray();
-        document.getElementById("main").style.display = 'none';
-        document.getElementById("holdQuestions").style.display = 'block';
-        secondsLeft = 60;
-        setTime();
-        quizBegins();
-        // This is where we place the secondsLeft = highscore variable. 
-        // Also probably the localstorage?
-        //document.getElementById("main").style.display = 'block'
+startQuizBtn.addEventListener("click",function launchQuiz (event) {
+  event.preventDefault();
+  shuffleArray();
+  document.getElementById("main").style.display = 'none';
+  document.getElementById("holdQuestions").style.display = 'block';
+  secondsLeft = 60;
+  setTime();
+  quizBegins();
+ 
+  });
 
-       });
+function setTime() {
+// Sets interval in variable
+var timerInterval = setInterval(function() {
+  secondsLeft--;
+  timeCountdown.textContent = secondsLeft;
 
-       function setTime() {
-        // Sets interval in variable
-        var timerInterval = setInterval(function() {
-          secondsLeft--;
-          timeCountdown.textContent = secondsLeft;
-      
-          if (secondsLeft <= 0 && quizIndex !== 4) {
-            // Stops execution of action at set interval
-             clearInterval(timerInterval);
-             console.log("Ran out of time")
-            document.getElementById("holdQuestions").style.display = 'none'
-            document.getElementById("recordScore").style.display = 'block'
+  if (secondsLeft <= 0 && quizIndex !== 4) {
+    // Stops execution of action at set interval
+    //secondsLeft = 1
+      clearInterval(timerInterval);
+      console.log("Ran out of time")
+    document.getElementById("holdQuestions").style.display = 'none'
+    document.getElementById("recordScore").style.display = 'block'
 
-            quizIndex = 0;
+    quizIndex = 0;
+    // secondsLeft = 1;
 
-            localStorage.setItem("finalScore", JSON.stringify(secondsLeft) )
-            document.getElementById('finalScore').innerHTML = localStorage.getItem("finalScore");
+    localStorage.setItem("finalScore", JSON.stringify(secondsLeft) )
+    document.getElementById('finalScore').innerHTML = localStorage.getItem("finalScore");
 
-            document.getElementById('exampleList').innerHTML = "";
+    document.getElementById('exampleList').innerHTML = "";
+
+    document.getElementById('questionTitle').innerHTML = "";
+
+    return;
+  
+    } 
+
+  if (quizIndex === 4){
+    clearInterval(timerInterval);
+    quizIndex = 0;
+    return;
+    }
+  
+  }, 1000);
+}
+
+function quizBegins(){
+
+var i = quizIndex
+
+var answerList = document.getElementById("exampleList")
+
+document.getElementById('answerStatus').innerHTML = "";
+
+
+console.log(event.target)
+for (g = 0; g < 4 ; g++) {
     
-            document.getElementById('questionTitle').innerHTML = "";
-            
-           return;
-         
-          } 
+    document.getElementById('questionTitle').innerHTML = questionArray[i].thisIsTheQuestion;
 
-          if (quizIndex === 4){
-            clearInterval(timerInterval);
-            quizIndex = 0;
-            return;
-          }
-          // else {
-          //   clearInterval(timerInterval);
-          //   return;
-          // }
+    var liElement = document.createElement('li');
+
+    liElement.innerHTML = questionArray[i].possibleAnswers[g]
+
+    answerList.appendChild(liElement);
+    
+    liElement.addEventListener('click', function(event){
+
+      // Below conditional statement necessary for assigning the penalty
+      // of having selected the wrong answer
       
-        }, 1000);
+      if (event.target.textContent === questionArray[i].correctAnswer) {
+        console.log("That's it!");
+        document.getElementById('answerStatus').innerHTML = "Correct!";
+
+
+      } else { 
+      console.log("Nope");
+      secondsLeft = secondsLeft - 15;
+      document.getElementById('answerStatus').innerHTML = "Wrong!";
+
+    }
+
+      quizIndex = quizIndex + 1;
+      console.log(quizIndex)
+      
+      document.getElementById('exampleList').innerHTML = "";
+      document.getElementById('questionTitle').innerHTML = "";
+      
+      // This conditional statement will reset the page and provide the
+      // localStorage information.
+      if (secondsLeft <= 0) {
+        secondsLeft = 0;
       }
 
-      function quizBegins(){
-
-        var i = quizIndex
-    
-        var answerList = document.getElementById("exampleList")
-    
-        document.getElementById('answerStatus').innerHTML = "";
-    
-    
-        console.log(event.target)
-        for (g = 0; g < 4 ; g++) {
-            
-            document.getElementById('questionTitle').innerHTML = questionArray[i].thisIsTheQuestion;
-    
-            var liElement = document.createElement('li');
-    
-            liElement.innerHTML = questionArray[i].possibleAnswers[g]
-    
-            answerList.appendChild(liElement);
-            
-            liElement.addEventListener('click', function(event){
-    
-              // Below conditional statement necessary for assigning the penalty
-              // of having selected the wrong answer
-              
-              if (event.target.textContent === questionArray[i].correctAnswer) {
-                console.log("That's it!");
-                document.getElementById('answerStatus').innerHTML = "Correct!";
-    
-    
-              } else { 
-              console.log("Nope");
-              secondsLeft = secondsLeft - 15;
-              document.getElementById('answerStatus').innerHTML = "Wrong!";
-    
-            }
-    
-              quizIndex = quizIndex + 1;
-              console.log(quizIndex)
-              
-              document.getElementById('exampleList').innerHTML = "";
-              document.getElementById('questionTitle').innerHTML = "";
-              
-              // This conditional statement will reset the page and provide the
-              // localStorage information.
-    
-              // if (secondsLeft == 0) {
-              //   console.log("Ran out of time")
-                // document.getElementById('answerStatus').innerHTML = "";
-                // document.getElementById("holdQuestions").style.display = 'none';
-               // document.getElementById("recordScore").style.display = 'block';
-                // console.log(secondsLeft)
-                // localStorage.setItem("finalScore", JSON.stringify(secondsLeft) )
-                // document.getElementById('finalScore').innerHTML = localStorage.getItem("finalScore");
-                // quizIndex = 0;
-                // secondsLeft = 1;
-              //   return;
-              // }
-              // if (secondsLeft === 0){
-              //   quizIndex == 4;
-              // }
-    
-              if (quizIndex === 4) {
-                
-                document.getElementById('answerStatus').innerHTML = "";
-                document.getElementById("holdQuestions").style.display = 'none';
-                document.getElementById("recordScore").style.display = 'block';
-                console.log(secondsLeft)
-                localStorage.setItem("finalScore", JSON.stringify(secondsLeft) )
-                document.getElementById('finalScore').innerHTML = localStorage.getItem("finalScore");
-               // quizIndex = 0;
-                //secondsLeft = 1;
-
-                return;
-              } else {
-              quizBegins();
-              }
-            })
-           }
-       }  
-
-      submitInitialsBtn.addEventListener("click", function recordScore (event) {
-        event.preventDefault();
-        console.log("Initials Submitted")
+      if (quizIndex === 4) {
         
-        //This is where we are going to store the scores. 
-//          enteredInitials.innerHTML = "";
-        var initialsProvided = document.querySelector("#enteredInitials").value;
-        localStorage.setItem("enteredInitials", JSON.stringify(initialsProvided));
-        document.getElementById("recordScore").style.display = 'none';
-
-        if (initialsProvided === "") {
-          document.getElementById("main").style.display = 'block';
-          return;
+        document.getElementById('answerStatus').innerHTML = "";
+        document.getElementById("holdQuestions").style.display = 'none';
+        document.getElementById("recordScore").style.display = 'block';
+        console.log(secondsLeft)
+        localStorage.setItem("finalScore", JSON.stringify(secondsLeft) )
+        document.getElementById('finalScore').innerHTML = localStorage.getItem("finalScore");
+        return;
+        } else {
+          quizBegins();
         }
-
-        // Then below is where we reset the Form and show the block
-        // highscoreTracker.
-        document.getElementById("scoreForm").reset();
-        document.getElementById("highscoreTracker").style.display = 'block'
-
-        // Then, we need to create an li with the respective score and initals.
-        var scoreLi = document.createElement('li');
-        var userInitials = JSON.parse(localStorage.getItem("enteredInitials"));
-        var timeLeft = localStorage.getItem("finalScore");
-        scoreLi.innerHTML = userInitials +"       "+ timeLeft;
-        listOfScores.appendChild(scoreLi);
-
-
-      });
-
-      goBackBtn.addEventListener("click", function goBack (event){
-        event.preventDefault();
-        document.getElementById("main").style.display = 'block';
-        document.getElementById("highscoreTracker").style.display = 'none';
       })
+    }
+}  
 
-      clearHighscoresBtn.addEventListener("click", function clearHighscores(event){
-        event.preventDefault();
-        document.getElementById('listOfScores').innerHTML = "";
+submitInitialsBtn.addEventListener("click", function recordScore (event) {
+  event.preventDefault();
+  console.log("Initials Submitted")
+  
+  //This is where we are going to store the scores. 
+  //enteredInitials.innerHTML = "";
+  var initialsProvided = document.querySelector("#enteredInitials").value;
+  localStorage.setItem("enteredInitials", JSON.stringify(initialsProvided));
+  document.getElementById("recordScore").style.display = 'none';
 
-      })
+  if (initialsProvided === "") {
+    document.getElementById("main").style.display = 'block';
+    return;
+  }
 
-      viewHighscoresBtn.addEventListener("click", function viewHighscores(event){
-        event.preventDefault();
-        document.getElementById("main").style.display = 'none';
-        document.getElementById("highscoreTracker").style.display = 'block';
-      })
+  // Then below is where we reset the Form and show the block
+  // highscoreTracker.
+  document.getElementById("scoreForm").reset();
+  document.getElementById("highscoreTracker").style.display = 'block'
+
+  // Then, we need to create an li with the respective score and initals.
+  var scoreLi = document.createElement('li');
+  var userInitials = JSON.parse(localStorage.getItem("enteredInitials"));
+  var timeLeft = localStorage.getItem("finalScore");
+  scoreLi.innerHTML = userInitials +"       "+ timeLeft;
+  listOfScores.appendChild(scoreLi);
+
+
+});
+
+goBackBtn.addEventListener("click", function goBack (event){
+  event.preventDefault();
+  document.getElementById("main").style.display = 'block';
+  document.getElementById("highscoreTracker").style.display = 'none';
+})
+
+clearHighscoresBtn.addEventListener("click", function clearHighscores(event){
+  event.preventDefault();
+  document.getElementById('listOfScores').innerHTML = "";
+
+})
+
+viewHighscoresBtn.addEventListener("click", function viewHighscores(event){
+  event.preventDefault();
+  document.getElementById("main").style.display = 'none';
+  document.getElementById("highscoreTracker").style.display = 'block';
+})
